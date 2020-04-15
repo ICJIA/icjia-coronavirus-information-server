186a1071b042df256af2708695e4389feeb85a49
bobaekang <bkang.dev@gmail.com>
2020-04-15 08:45:33 -0500

    fix: trim whitespace in html

--- index.js
+++ index.js
@@ -14,31 +14,36 @@ app.use(cors());
 
 /* will use axios to fetch updated state content when necessary */
 
+const trimws = (str) => {
+  return str.raw[0].replace(/\s+/g, " ").replace(/\s+>/g, ">").trim();
+};
+
 const content = {
-  html: `<i aria-hidden="true" class="v-icon notranslate material-icons theme--light red--text">error</i>
+  html: trimws`
+    <i aria-hidden="true" class="v-icon notranslate material-icons theme--light red--text">error</i>
     View up to date information on how Illinois is handling the Coronavirus Disease 2019 (COVID-19) from the <a href="https://coronavirus.illinois.gov" target="_blank" style="text-decoration: underline;">State of Illinois Coronavirus Response Site</a>
-    `,
+  `,
 };
 
 const contentV2 = {
-  html: `
-      <div class="pl-3" style="color: #444">
-        View up to date information on how Illinois is handling the Coronavirus Disease 2019 (COVID-19) from the <a
-          href="https://coronavirus.illinois.gov"
-          style="text-decoration: underline;"
-          target="_blank"
-          >State of Illinois Coronavirus Response Site</a
-        >.
-      </div>
-      <div class="pl-3 mt-1" style="color: #444">
-        View <a
-          href="https://icjia.illinois.gov/covid19"
-          target="_blank"
-          style="text-decoration: underline;"
-          >ICJIA's COVID-19 Response page</a
-        > for the latest grant and fiscal notifications, resources, and more.
-      </div>
-   `,
+  html: trimws`
+    <div class="pl-3" style="color: #444">
+      View up to date information on how Illinois is handling the Coronavirus Disease 2019 (COVID-19) from the <a
+        href="https://coronavirus.illinois.gov"
+        style="text-decoration: underline;"
+        target="_blank"
+        >State of Illinois Coronavirus Response Site</a
+      >.
+    </div>
+    <div class="pl-3 mt-1" style="color: #444">
+      View <a
+        href="https://icjia.illinois.gov/covid19"
+        target="_blank"
+        style="text-decoration: underline;"
+        >ICJIA's COVID-19 Response page</a
+      > for the latest grant and fiscal notifications, resources, and more.
+    </div>
+  `,
 };
 
 app.get("/", (req, res) => res.send(content));
